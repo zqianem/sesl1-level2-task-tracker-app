3cf02759b23740e99adbe48b6327bdfbc2c30ee1
Em Zhan <zqianem@gmail.com>
2023-04-21 17:17:30 -0500

    Store user_id in cookie and use it for queries

--- src/routes/tasks/+page.server.js
+++ src/routes/tasks/+page.server.js
@@ -2,10 +2,11 @@ import { error as sk_error } from '@sveltejs/kit';
 
 export async function load({ locals: { supabase }, cookies }) {
   const username = cookies.get('username');
+  const user_id = cookies.get('user_id');
   const { data, error } = await supabase
     .from('tasks')
-    .select('*, users!inner(*)')
-    .eq('users.username', username);
+    .select()
+    .eq('user_id', user_id);
 
   if (error) throw sk_error(500, error);
 
