bbac5d7c0fa6d261edaa3b2539cec003a8c8cd14
k890m <94244306+k890m@users.noreply.github.com>
2023-04-21 10:45:28 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,23 @@
 <script>
+    export let data;
+    let {users} = data
     let username = "";
-
-    const login = async () => {
+    
+    async function check() {
         let { data: users, error } = await supabase
             .from('users')
             .select('username')
-            .eq("username", username)
-            .single();
-
-        if(error){
-            console.error(error);
-            return;
+        
+        if(!data){
+               const { data, error } = await supabase
+                    .from('users')
+                    .insert([ ,{ username: 'username'}])
         }
-
-        if (data) {
-            localStorage.setItem("user_id", data.id);
-        } else {
-            const { data, error } = await supabase.from("users").insert({ username });
-            if (error) {
-                console.error(error);
-                return;
-            }
-            localStorage.setItem("id", data.id);
-        }
-
+        
         window.location.href = "/tasks";
-    };
-
+    }
+    
+    
 </script>
 
 
@@ -34,7 +25,7 @@
     <h1>Get tasks</h1>
     <form action="tasks">
         <input type="text" bind:value={username} placeholder="Enter name" />
-        <button onclick="login" type="submit">Submit</button>
+        <button onclick="check" type="submit">Submit</button>
     </form>
 </main>
 
