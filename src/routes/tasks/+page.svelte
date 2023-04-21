acef51367921ceb54ba80aa44ebc65f43bd23a26
Nikhil Ganjikunta <nganjiku@sundevils.asu.edu>
2023-04-20 23:19:59 -0700

    Resolve comments

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,25 +1,25 @@
 <script>
     export let data;
-    console.log(data)
 
-  </script>
 
-  <div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-	<div class="bg-slate-100 rounded shadow p-6 m-4 w-full">
+</script>
+
+<div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+    <div class="bg-slate-100 rounded shadow p-6 m-4 w-full">
         <div class="mb-4">
             <h2 class="text-center font-bold p-4">Create Task</h2>
             <div class="grid grid-cols-1 justify-center">
                 <div class="p-1">
-                    <p>Title</p>
-                    <input class="shadow appearance-none border rounded w-1/2 py-2 px-3 mr-4 text-grey-darker" placeholder="Title">
-                </div>
-                <div >
-                    <p>Description</p>
-                    <input class="shadow appearance-none border rounded w-1/2 py-2 px-3 mr-4 text-grey-darker" placeholder="Description">
+                    <label for="title">Title</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"name="title" id="title">
                 </div>
                 <div class="p-1">
-                    <p>Due Date</p>
-                    <input class="shadow appearance-none border rounded w-1/2 py-2 px-3 mr-4 text-grey-darker"  type="date" />
+                    <label for="desc">Description</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"name="desc" id="desc">
+                </div>
+                <div class="p-1">
+                    <label for="ddate">Due Date</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"name="ddate" id="ddate" type="date">
                 </div>
             </div>
             
@@ -28,15 +28,15 @@
             Create Task
         </button>
     </div>
-  </div>
-  <div class="w-full bg-slate-100 rounded shadow p-6">
+</div>
+<div class="w-full bg-slate-100 rounded shadow p-6">
     <h2
     class="text-center font-bold"> Tasks List</h2>
     {#if data.tasks.length > 0}
     <ul>
         {#each data.tasks as {title,description,status,due_date}}
         <li>
-            <div class="w-full rounded shadow m-4 p-6">
+            <div class="rounded shadow m-4 p-6">
                 <h3 class="font-bold">{title}</h3>
                 <p>{description}</p>
                 <div>
@@ -53,5 +53,5 @@
         {/each}
     </ul>
     {/if}
-  </div>
+</div>
 
