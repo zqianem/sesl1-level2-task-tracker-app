4d287250e24d63ed0ba1affb80607fde428042a3
k890m <94244306+k890m@users.noreply.github.com>
2023-04-21 11:12:04 -0400

    Update +page.svelte.js

--- src/routes/+page.svelte.js
+++ src/routes/+page.svelte.js
@@ -3,7 +3,7 @@ import { error as sk_error } from '@sveltejs/kit';
 export async function load({ locals: { supabase } }) {
   const { data, error } = await supabase
     .from('users')
-    .select();
+    .select('*');
 
   if (error) throw sk_error(500, error);
   return {"users":data};
