b65f7db61a004bc08dcede34f15b5f602080b83a
k890m <94244306+k890m@users.noreply.github.com>
2023-04-21 16:32:54 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,30 @@
 <script>
+  import Cookies from 'js-cookie';
+
   export let data;
   let username = "";
+  
 
   const checkUser = async (event) => {
     let { data: users, error } = await supabase.from('users').select('username')
+    
 
     if (!users) {
       const { data, error } = await supabase.from('users').insert([{ username: 'username' }])
+      const user = data[0];
+      Cookies.set('user_id', user.id);
+    } else {
+      const user = users[0];
+      Cookies.set('user_id', user.id);
     }
+
+    window.location.href = "/tasks";
   }
   
 </script>
 
 <h1>Get tasks</h1>
-<form method="POST" action="tasks">
+<form action="tasks">
   <input type="text" bind:value={username} placeholder="Enter name" />
   <button on:click={checkUser} type="submit">Submit</button>
 </form>
